8563218d850866bb799b2ea7545e7de4698e0d31
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-26 20:35:51 +0530

    search bar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React,{useState} from "react";
+import axios from "axios";
 
 function App() {
+const[data,setData]=useState({});
+const [location,setLocation]=useState('')
 
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=bdc1146c700b7ab3cbbd22c72a75063f";
+const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=bdc1146c700b7ab3cbbd22c72a75063f`;
 
+const searchLocation = (event)=>{
+  if(event.key==="Enter"){
+    axios.get(url).then((response)=>{
+    setData(response.data)
+    console.log(response.data);
+  })
+}
+  
+}
 
   return (
     <div className="app">
+    <div className="search">
+    <input type="text" onChange={event=>setLocation(event.target.value)}
+    placeholder="Enter Location" />
+    </div>
       <div className="container">
         <div className="top">
           <div className="location">
