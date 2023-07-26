adaa23536e9f14070c225584bf08ddd4eab42907
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-26 22:14:08 +0530

    api temp unit updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 const[data,setData]=useState({});
 const [location,setLocation]=useState('')
 
-const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=bdc1146c700b7ab3cbbd22c72a75063f`;
+const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=bdc1146c700b7ab3cbbd22c72a75063f`;
 
 const searchLocation = (event)=>{
   if(event.key==="Enter"){
@@ -19,7 +19,6 @@ const searchLocation = (event)=>{
 }
 
   return (
-
     <div className="app">
       <div className="search">
         <input
@@ -34,27 +33,31 @@ const searchLocation = (event)=>{
           <div className="location">
             <p>{data.name}</p>
           </div>
-          <div className="temp">{data.main && <h1>{data.main.temp}°F</h1>}</div>
+          <div className="temp">
+            {data.main && <h1>{data.main.temp.toFixed()}°F</h1>}
+          </div>
           <div className="description">
             {data.weather && <p>{data.weather[0].main}°F</p>}
           </div>
         </div>
-        {data.name !== undefined &&
-        <div className="bottom">
-          <div className="feels">
-            {data.main && <p className="bold">{data.main.feels_like}</p>}
-            <p>Feels Like</p>
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main && (
+                <p className="bold">{data.main.feels_like.toFixed()}</p>
+              )}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main && <p className="bold">{data.main.humidity}</p>}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind && <p className="bold">{data.wind.speed}</p>}
+              <p>Wind speed</p>
+            </div>
           </div>
-          <div className="humidity">
-            {data.main && <p className="bold">{data.main.humidity}</p>}
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            {data.wind && <p className="bold">{data.wind.speed}</p>}
-            <p>Wind speed</p>
-          </div>
-        </div>
-        }
+        )}
       </div>
     </div>
   );
