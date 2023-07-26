ab6b9f59f587af6305341634c96c06619f670ec3
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-26 22:16:00 +0530

    unit updated

--- src/App.js
+++ src/App.js
@@ -44,16 +44,18 @@ const searchLocation = (event)=>{
           <div className="bottom">
             <div className="feels">
               {data.main && (
-                <p className="bold">{data.main.feels_like.toFixed()}</p>
+                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
               )}
               <p>Feels Like</p>
             </div>
             <div className="humidity">
-              {data.main && <p className="bold">{data.main.humidity}</p>}
+              {data.main && <p className="bold">{data.main.humidity}%</p>}
               <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind && <p className="bold">{data.wind.speed}</p>}
+              {data.wind && (
+                <p className="bold">{data.wind.speed.toFixed()}MPH</p>
+              )}
               <p>Wind speed</p>
             </div>
           </div>
