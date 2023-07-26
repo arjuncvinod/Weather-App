c26e810c1414ad406141941738067d43f44c7a78
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-26 19:54:29 +0530

    styles added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 
 function App() {
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=bdc1146c700b7ab3cbbd22c72a75063f";
+
+
   return (
-    <h1>hello</h1>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">65°F</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20% </p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Wind speed</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
   );
 }
 
